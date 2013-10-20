e06d3dd399d044c21ecc1d297fa8f3590474cec1
calltella <yuichi.asa@gmail.com>
2013-10-20 13:09:09 +0900

    日本語ファイルを追加

---
+++ lang/ja/lang.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Japanese Language File
+ * @author calltella <centos@calltella.com>
+ */
+ 
+$lang['current']    = '今日';
+$lang['go']         = '移動';
+
+$lang['year']       = '年';
+$lang['mon']        = '月';
+ 
+$lang['days'][1]    = '月';
+$lang['days'][2]    = '火';
+$lang['days'][3]    = '水';
+$lang['days'][4]    = '木';
+$lang['days'][5]    = '金';
+$lang['days'][6]    = '土';
+$lang['days'][7]    = '日';
+ 
+$lang['month'][1]   = '01';
+$lang['month'][2]   = '02';
+$lang['month'][3]   = '03';
+$lang['month'][4]   = '04';
+$lang['month'][5]   = '05';
+$lang['month'][6]   = '06';
+$lang['month'][7]   = '07';
+$lang['month'][8]   = '08';
+$lang['month'][9]   = '09';
+$lang['month'][10]  = '10';
+$lang['month'][11]  = '11';
+$lang['month'][12]  = '12';
+
+// vim:ts=4:sw=4:et:enc=utf-8:
---
+++ lang/ja/settings.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Japanese settings file for DokuWiki Plugin wikicalendar
+ * @author calltella <centos@calltella.com>
+ */
+$lang['weekstart'] = '最初の週';
+$lang['timezone']  = 'タイムゾーン(デフォルトでサーバーのタイムゾーンを取得)';
